cbb4d50a9862d37c849c9f0db3887f380ff50b5c
skmbw <tongku2008@126.com>
2020-12-31 16:59:29 +0800

    添加常用的angular模组

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,26 @@ import {InvestModule} from './invest/invest.module';
 import {StockModule} from './stock/stock.module';
 import {RoutingModule} from './routing/routing.module';
 import {ToastrModule} from 'ngx-toastr';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCommonModule, MatOptionModule} from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {FroalaEditorModule, FroalaViewModule} from 'angular-froala-wysiwyg';
 
 @NgModule({
   declarations: [
@@ -21,6 +41,32 @@ import {ToastrModule} from 'ngx-toastr';
     StockModule,
     RoutingModule,
     ToastrModule.forRoot({timeOut: 3000, positionClass: 'toast-center-center'}),
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatCommonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
+    // 如果不在这里配置，chrome会提示脏检查错误
+    ToastrModule.forRoot({timeOut: 3000, positionClass: 'toast-center-center'}),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
