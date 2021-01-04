57d746d45f0b983f2296194f8a76c5cfd5484d09
skmbw <tongku2008@126.com>
2021-01-04 15:31:29 +0800

    展示账户资产

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,15 +4,17 @@ import {MainComponent} from './main/main.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import {InvestModule} from '../invest/invest.module';
 import {StockModule} from '../stock/stock.module';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [
-    CommonModule,
-    MatTabsModule,
-    InvestModule,
-    StockModule
-  ]
+    imports: [
+        CommonModule,
+        MatTabsModule,
+        InvestModule,
+        StockModule,
+        MatCardModule
+    ]
 })
 export class DashboardModule { }
