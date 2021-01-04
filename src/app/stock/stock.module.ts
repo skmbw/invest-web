5a2136d0e742ba2c703eb276861615ce721a3dbc
skmbw <tongku2008@126.com>
2021-01-04 17:26:02 +0800

    分割线不起作用

--- src/app/stock/stock.module.ts
+++ src/app/stock/stock.module.ts
@@ -3,16 +3,19 @@ import {CommonModule} from '@angular/common';
 import {StockListComponent} from './stock-list/stock-list.component';
 import {MatListModule} from '@angular/material/list';
 import {EvaluateComponent} from './evaluate/evaluate.component';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
-    declarations: [StockListComponent, EvaluateComponent],
-    exports: [
-        StockListComponent
-    ],
-    imports: [
-        CommonModule,
-        MatListModule
-    ]
+  declarations: [StockListComponent, EvaluateComponent],
+  exports: [
+    StockListComponent
+  ],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatDividerModule
+  ]
 })
-export class StockModule { }
+export class StockModule {
+}
