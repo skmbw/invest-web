f81c25fdbf95f7a689a6eaf85f427857eae5ed53
skmbw <tongku2008@126.com>
2021-01-05 16:02:12 +0800

    这样的话，跟单独列表头没有什么区别了

--- src/app/stock/stock-list/stock-list.component.ts
+++ src/app/stock/stock-list/stock-list.component.ts
@@ -7,26 +7,6 @@ import {EvaluateComponent} from '../evaluate/evaluate.component';
 import {ToastrService} from 'ngx-toastr';
 import {EvaluationResult} from '../../bean/evaluation.result';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html',
@@ -36,12 +16,9 @@ export class StockListComponent implements OnInit {
   stockList: Stock[] = [];
   displayedColumns: string[][] = [['name', '股票名称'],
     ['code', '股票代码'], ['currentPrice', '价格'],
-    ['increaseRate', '涨幅'], ['marketValue', '市值']];
-  chineseColumnsToDisplay: string[] = ['股票名称', '股票代码', '价格', '涨幅'];
-  // columnsToDisplay: string[] = this.displayedColumns.slice();
+    ['increaseRate', '涨幅'], ['marketValue', '市值（亿）']];
   dataColumns = ['name', 'code', 'currentPrice', 'increaseRate', 'marketValue'];
-  // displayedColumns: string[] = this.columnsToDisplay.slice();
-  data: PeriodicElement[] = ELEMENT_DATA;
+
   constructor(private stockService: StockService, private investService: InvestService,
               private dialog: MatDialog, private toastrService: ToastrService) {
   }
