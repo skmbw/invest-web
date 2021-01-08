6925bf073c62bf6c17c5769542462e28db2b3f8d
skmbw <tongku2008@126.com>
2021-01-08 10:30:44 +0800

    实现转出和转入业务逻辑

--- src/app/dashboard/capital-inout/capital-inout.component.ts
+++ src/app/dashboard/capital-inout/capital-inout.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Account} from '../../bean/account';
+import {AccountService} from '../../service/account.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-capital-inout',
@@ -8,11 +10,38 @@ import {Account} from '../../bean/account';
   styleUrls: ['./capital-inout.component.css']
 })
 export class CapitalInoutComponent implements OnInit {
+  title = '转入';
+  account = new Account();
+  operateList = [{name: '资金转入', state: 1}, {name: '资金转出', state: 2}];
 
-  constructor(private matDialogRef: MatDialogRef<CapitalInoutComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Account) { }
+  constructor(private matDialogRef: MatDialogRef<CapitalInoutComponent>, @Inject(MAT_DIALOG_DATA) public data: Account,
+              private accountService: AccountService, private toastr: ToastrService) {
+    if (data.state === 2) {
+      this.account.state = 2;
+      this.title = '转出';
+    } else {
+      this.account.state = 1;
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  transfer() {
+    if (this.account.state == null) {
+      this.toastr.success('请选择操作类型。');
+      return;
+    }
+    if (this.account.capital == null || this.account.capital === 0) {
+      this.toastr.success('请填写资金数额。');
+      return;
+    }
+    this.accountService.capitalTransfer(this.account).subscribe(result => {
+      if (result.code === 1) {
+        this.toastr.success('资金操作成功。');
+      } else {
+        this.toastr.success(result.message);
+      }
+    });
+  }
 }
--- src/app/service/account.service.ts
+++ src/app/service/account.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {JsonBean} from '../bean/jsonbean';
+import {Account} from '../bean/account';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,13 @@ export class AccountService {
 
   list(): Observable<JsonBean> {
     return this.httpClient.post<JsonBean>(environment.host + 'stockAccount/list', {name: '尹雷'});
+  }
+
+  capitalTransfer(account: Account): Observable<JsonBean> {
+    let url = 'stockAccount/transferIn';
+    if (account.state === 2) {
+      url = 'stockAccount/transferOut';
+    }
+    return this.httpClient.post<JsonBean>(environment.host + url, account);
   }
 }
